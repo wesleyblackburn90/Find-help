ae109ad867777ccd82e121e2ae671f6fda3958fb
Wesley Blackburn <wesley.blackburn90@yahoo.com>
2022-06-27 19:05:34 -0400

    added business path

---
+++ frontend/src/components/BusinessList/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import { useDispatch } from 'react-redux';
+// import * as sessionActions from '../../store/session';
+// import { Redirect } from 'react-router-dom';
+import './BusinessList.css';
+
+function BusinessList() {
+  return (
+    <h1>Hello</h1>
+  )
+}
+
+export default BusinessList;
